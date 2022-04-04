f2a0711ba47a0cf2d03f98fba23470382b4871f7
Nandhini0101 <100352008+Nandhini0101@users.noreply.github.com>
2022-04-04 10:47:10 +0530

    Add files via upload

---
+++ T2.ps1
@@ -0,0 +1,12 @@
+﻿$Number = Read-Host -Prompt "Enter your range to find fibonacci"
+$n1 = 0
+$n2 = 1
+$n3 = 0
+Write-Host $n1 
+for ($i = 3 ; $i -le $Number ; $i++)
+{
+ $n3 = $n1 + $n2
+ Write-Host $n3 
+ $n1 = $n2
+ $n2 = $n3 
+ }
