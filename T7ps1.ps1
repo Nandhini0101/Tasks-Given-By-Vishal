9d296695b54a061843ee630994c19e5248aaf6d3
Nandhini0101 <100352008+Nandhini0101@users.noreply.github.com>
2022-04-04 10:49:30 +0530

    Add files via upload

---
+++ T7ps1.ps1
@@ -0,0 +1,17 @@
+﻿$Number = Read-Host -Prompt "Enter your Number"
+$Original = $Number
+$Reverse = 0
+while($Number -gt 0)
+{ 
+$r = $Number % 10 
+$Reverse = $Reverse * 10 +$r 
+$Number = $Number/10 
+}
+if($Original -eq $Reverse) 
+{ 
+Write-Host $Number " is a Palindrome Number" 
+ }
+else
+{ 
+ Write-Host $Number " is not a Palindrome Number" 
+}
